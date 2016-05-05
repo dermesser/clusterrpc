8ae76930a17bad45146d6194b73845beadc0d1ba
Lewin Bormann <lbo@spheniscida.de>
2016-05-05 19:14:26 +0200

    server/queue: Implement array based fast generic queue

---
+++ server/queue/queue.go
@@ -0,0 +1,51 @@
+package queue
+
+// An array-based fixed-length queue implementation, supposedly faster than a LinkedList implementation.
+// Used for queuing both workers and requests.
+
+type genericQueue struct {
+	front, back, l int
+	queue          []interface{}
+}
+
+func newQueue(len int) genericQueue {
+	return genericQueue{front: 0, back: 0, l: 0, queue: make([]interface{}, len)}
+}
+
+func (q *genericQueue) len() int {
+	return q.l
+}
+
+// Append to the back. Returns false if queue is full.
+func (q *genericQueue) push(e interface{}) bool {
+	if q.len() < len(q.queue) {
+		q.queue[q.back] = e
+		q.back = (q.back + 1) % len(q.queue)
+		q.l++
+		return true
+	} else {
+		return false
+	}
+}
+
+// Get from the front. May return nil if queue is empty.
+func (q *genericQueue) pop() interface{} {
+	if q.len() > 0 {
+		e := q.queue[q.front]
+		q.queue[q.front] = nil
+		q.front = (q.front + 1) % len(q.queue)
+		q.l--
+		return e
+	} else {
+		return nil
+	}
+}
+
+// Returns the front element without removing it.
+func (q *genericQueue) peek() interface{} {
+	if q.len() > 0 {
+		return q.queue[q.front]
+	} else {
+		return false
+	}
+}
---
+++ server/queue/queue_test.go
@@ -0,0 +1,116 @@
+package queue
+
+import "testing"
+import "container/list"
+
+func TestPush(t *testing.T) {
+	q := newQueue(3)
+
+	a, b, c := q.push(3), q.push(4), q.push(5)
+
+	if !(a && b && c) {
+		t.Fatal("Could not push three elements")
+	}
+}
+
+func TestPushLimit(t *testing.T) {
+	q := newQueue(2)
+
+	a, b, c := q.push(3), q.push(4), q.push(5)
+
+	if !(a && b) && c {
+		t.Fatal("Could push last element:", a, b, c)
+	}
+}
+
+func TestPopEmpty(t *testing.T) {
+	q := newQueue(10)
+
+	if nil != q.pop() {
+		t.Fatal("Could pop from empty queue")
+	}
+}
+
+func TestPop(t *testing.T) {
+	q := newQueue(10)
+
+	q.push(1)
+	q.push(2)
+	q.push(3)
+
+	a, b, c := q.pop().(int), q.pop().(int), q.pop().(int)
+
+	if a != 1 || b != 2 || c != 3 {
+		t.Fatal("Bad contents:", a, b, c)
+	}
+
+	if nil != q.pop() {
+		t.Fatal("Yields element past end")
+	}
+}
+
+func TestLen(t *testing.T) {
+	q := newQueue(10)
+	q.push(2)
+	q.push(3)
+	if q.len() != 2 {
+		t.Fatal("Wrong length", q.len())
+	}
+}
+func TestLen2(t *testing.T) {
+	q := newQueue(3)
+	q.push(2)
+	q.push(3)
+	q.push(4)
+	q.pop()
+	q.pop()
+	q.push(5)
+	if q.len() != 2 {
+		t.Fatal("Wrong length", q.len())
+	}
+}
+
+func TestPeek(t *testing.T) {
+	q := newQueue(10)
+	q.push(2)
+
+	if 2 != q.peek().(int) {
+		t.Fatal("Wrong element:", q.peek().(int))
+	}
+}
+
+// Benches time for pushing, then popping 10 elements from a long queue
+func BenchmarkQueue(b *testing.B) {
+	q := newQueue(1000)
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 10; j++ {
+			if !q.push(j) {
+				b.Fatal("couldn't push")
+			}
+		}
+		for j := 0; j < 10; j++ {
+			if nil == q.pop() {
+				b.Fatal("got nil", i, j)
+			}
+		}
+	}
+}
+
+// Compare with linked list performance
+func BenchmarkLinkedList(b *testing.B) {
+	l := list.New()
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 10; j++ {
+			if nil == l.PushBack(i) {
+				b.Fatal("couldn't push")
+			}
+		}
+		for j := 0; j < 10; j++ {
+			if nil == l.Remove(l.Front()) {
+				b.Fatal("got nil", i, j)
+			}
+		}
+	}
+}
